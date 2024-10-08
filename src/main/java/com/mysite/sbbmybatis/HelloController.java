07c40eea3080df7f080ecc531e163dd21d36c162
Austine <chucky891213@naver.com>
2024-10-08 09:23:06 +0900

    MR: feat/login 프로젝트 시작

--- src/main/java/com/mysite/sbbmybatis/HelloController.java
+++ src/main/java/com/mysite/sbbmybatis/HelloController.java
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-//    @ResponseBody
-    public String hello(Model model) {
-        model.addAttribute("message", "오늘도 나는 Hello World를 친다...^^");
-        model.addAttribute("comment", "야 너도...?");
+    public String hello() {
     	return "hello";
     }
     
-    @GetMapping("/bye")
-    @ResponseBody
-    public String bye() {
-    	return "Hello World... Bye";
+    @GetMapping("/user/login")
+    public String login() {
+
+    	return "/user/login";
     }
+    
+
 }
