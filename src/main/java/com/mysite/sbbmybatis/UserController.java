dc2721807f51f11daff8001ea581729386efe90a
Austine <chucky891213@naver.com>
2024-10-08 16:52:30 +0900

    MR: 데이터베이스에 INSERT하기(Controller 작동 확인)

--- src/main/java/com/mysite/sbbmybatis/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
@@ -16,6 +19,12 @@ public class UserController {
 		return "/user/login";
 	}
 
+	@PostMapping("/user/login")
+	@ResponseBody
+	public void login2(@RequestBody User user) {
+		User newUser = user;
+	}
+	
 	@GetMapping("/user/{usrname}")
 //    @ResponseBody
 	public String getUser(@PathVariable("usrname") String usrname, Model model) {
