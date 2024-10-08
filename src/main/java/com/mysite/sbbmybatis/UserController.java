9377151dad4829deccdc02d1c6e5f5d729c5b0f2
Austine <chucky891213@naver.com>
2024-10-08 16:09:34 +0900

    MR: Model이용해서 detail.html파일 return하기

--- src/main/java/com/mysite/sbbmybatis/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/UserController.java
@@ -2,25 +2,25 @@ package com.mysite.sbbmybatis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 	@Autowired
 	private UserService service;
 
-    
-    @GetMapping("/user/login")
-    public String login() {
-    	return "/user/login";
-    }
+	@GetMapping("/user/login")
+	public String login() {
+		return "/user/login";
+	}
 
-	
-    @GetMapping("/user/{usrname}")
-    @ResponseBody
-    public User getUser(@PathVariable("usrname") String usrname) {
-    	return service.getUser(usrname);
-    }
+	@GetMapping("/user/{usrname}")
+//    @ResponseBody
+	public String getUser(@PathVariable("usrname") String usrname, Model model) {
+		User user = service.getUser(usrname);
+		model.addAttribute("user", user);
+		return "/user/detail";
+	}
 }
