47def53c38c55137039469f81a4f1ed0f85a8638
Austine <chucky891213@naver.com>
2024-10-11 10:16:46 +0900

    MR: pagination 구현

--- src/main/java/com/mysite/sbbmybatis/MessageMapper.java
+++ src/main/java/com/mysite/sbbmybatis/MessageMapper.java
@@ -3,6 +3,7 @@ package com.mysite.sbbmybatis;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface MessageMapper {
@@ -11,4 +12,7 @@ public interface MessageMapper {
 	void insertMessage(Message message);
 
 	List<Message> getMessagesLimit10(); // return 값이 하나가 아님 -> List<>사용 -> <Message>타입만 사용하겠다!
+	
+	List<Message> getMessagesWithPaging(@Param("size") Integer size, @Param("offset") Integer offset);
+	int countTotal();
 }
---
+++ src/main/java/com/mysite/sbbmybatis/PageDTO.java
@@ -0,0 +1,60 @@
+package com.mysite.sbbmybatis;
+
+import java.util.List;
+
+public class PageDTO {
+	private int page;
+	private int size; // 한 페이지에 보여줄 메시지 리스트 수 -> 여기서는 10개 보여줌
+	private int totalPages;
+	private long totalElements;
+	private List<Message> content;
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public long getTotalElements() {
+		return totalElements;
+	}
+
+	public void setTotalElements(long totalElements) {
+		this.totalElements = totalElements;
+	}
+
+	public List<Message> getContent() {
+		return content;
+	}
+
+	public void setContent(List<Message> content) {
+		this.content = content;
+	}
+
+	public PageDTO(int page, int size, int totalPages, long totalElements, List<Message> content) {
+		super();
+		this.page = page;
+		this.size = size;
+		this.totalPages = totalPages;
+		this.totalElements = totalElements;
+		this.content = content;
+	}
+}
