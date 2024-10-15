2bca3b831794e1b9de3f371a00b0f802b3ba5b47
Austine <chucky891213@naver.com>
2024-10-15 11:28:09 +0900

    MR: 회원가입 작성(signup.html)

---
+++ src/main/java/com/mysite/sbbmybatis/user/UserController.java
@@ -0,0 +1,19 @@
+package com.mysite.sbbmybatis.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	// GetMapping /user/signup
+	@GetMapping("/signup")
+	public String signup() {
+		return "/user/signup";
+	}
+	
+	// PostMapping /user/signup
+	// GetMapping /user/login
+	// PostMapping /user/login
+}
