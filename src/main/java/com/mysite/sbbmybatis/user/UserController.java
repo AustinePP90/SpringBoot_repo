ab4eb94870e62e9fc2e598001fa60129173d29d2
Austine <chucky891213@naver.com>
2024-10-15 11:50:18 +0900

    MR: 컨트롤러에 GetMapping, PostMapping 작성

--- src/main/java/com/mysite/sbbmybatis/user/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/user/UserController.java
@@ -2,6 +2,8 @@ package com.mysite.sbbmybatis.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,11 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 	// GetMapping /user/signup
 	@GetMapping("/signup")
-	public String signup() {
+	public String signupPage() {
 		return "/user/signup";
 	}
-	
+
 	// PostMapping /user/signup
+	@PostMapping("/signup")
+	public void signup(@ModelAttribute("user") User user) {
+		System.out.println(user.toString());
+	}
+
 	// GetMapping /user/login
 	// PostMapping /user/login
 }
